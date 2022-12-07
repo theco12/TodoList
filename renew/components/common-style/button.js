89daf0b4ec9060e1c56de48b7cd3523c2f5735e0
theco12 <107103821+theco12@users.noreply.github.com>
2022-12-07 18:59:54 +0900

    update

--- renew/components/common-style/button.js
+++ renew/components/common-style/button.js
@@ -4,7 +4,8 @@ const Button = styled.div`
   width: 160px;
   height: 50px;
   border-radius: 50px;
-  padding: 15px 35px;
+  text-align: center;
+  padding: 15px 0;
   font-size: 14px;
   margin: 0 10px;
   cursor: pointer;
