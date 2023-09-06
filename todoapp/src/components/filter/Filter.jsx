12cd9ba89321fec7106494cfeb317f5b56e21a2f
theco12 <107103821+theco12@users.noreply.github.com>
2023-09-06 17:30:19 +0900

    update

--- todoapp/src/components/filter/Filter.jsx
+++ todoapp/src/components/filter/Filter.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import styles from "./Filter.module.css";
 
 export default function Filter({ filters, filter, onchangeFilter }) {
   return (
-    <ul>
-      {filters.map((value, index) => (
-        <li key={index}>
-          <button
-            onClick={() => {
-              onchangeFilter(value);
-            }}>
-            {value}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <header className={styles.header}>
+      <ul className={styles.filters}>
+        {filters.map((value, index) => (
+          <li key={index} className={styles.filter}>
+            <button
+              onClick={() => {
+                onchangeFilter(value);
+              }}>
+              {value}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </header>
   );
 }
